5888c08cc5676e3f46e8d1c42819a2638bf8add7
shahana khundmir <58731479+shahanakhundmir@users.noreply.github.com>
2025-11-19 06:09:29 +0000

    remove some console logs

--- frontend/src/api/summary.ts
+++ frontend/src/api/summary.ts
@@ -8,15 +8,12 @@ export async function fetchSummaryReport(
   const url = `http://127.0.0.1:8000/summary/report?start_date=${encodeURIComponent(
     startDate
   )}&end_date=${encodeURIComponent(endDate)}`;
-    console.log("url", url);
   const res = await fetch(url);
 
-  console.log("url", url);
 
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`);
   }
-  console.log('res',res)
 
   return res.json();
 }
